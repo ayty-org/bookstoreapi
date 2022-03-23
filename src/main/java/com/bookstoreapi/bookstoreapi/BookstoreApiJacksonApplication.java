ce44b7b99524abd3c0ce3086539496ab33c97faf
unknown <jacksonanderson1515@hotmail.com>
2022-03-23 18:39:27 -0300

    initial commit

---
+++ src/main/java/com/bookstoreapi/bookstoreapi/BookstoreApiJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.bookstoreapi.bookstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiJacksonApplication.class, args);
+	}
+
+}
