0cd13fbde0458488450c57580d559c56111f8b51
unknown <jacksonanderson1515@hotmail.com>
2022-03-24 17:27:42 -0300

    refactoring in exceptions

--- src/main/java/com/bookstoreapi/bookstoreapi/user/UserController.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 
@@ -45,4 +46,15 @@ public class UserController {
     public void delete(@PathVariable Long userId){
         userService.delete(userId);
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> entityNotFound(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegalArgument(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
 }
--- src/main/java/com/bookstoreapi/bookstoreapi/user/UserFieldsVerification.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/user/UserFieldsVerification.java
@@ -1,12 +1,11 @@
 package com.bookstoreapi.bookstoreapi.user;
 
-import com.bookstoreapi.bookstoreapi.exceptions.IllegalArgumentException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserFieldsVerification {
 
-    String[] validGenders = {"Male", "Female", "Transgender woman", "Transgender man", "Other"};
+    String[] validGenders = {"Male", "Female", "Other"};
 
 
     public void userFieldsAreValid(User user){
@@ -18,29 +17,29 @@ public class UserFieldsVerification {
 
     private void emailIsValid(String email){
         if(!email.contains("@")){
-            throw new IllegalArgumentException("email is invalid");
+            throw new IllegalArgumentException("email invalid");
         }
     }
 
     private void genderIsValid(String gender){
         for(String validGender: validGenders){
-            if(gender.equals(validGender)){
+            if(gender.equalsIgnoreCase(validGender)){
                 return;
             }
         }
-        throw new IllegalArgumentException("gender is invalid");
+        throw new IllegalArgumentException("gender invalid");
 
     }
 
     private void telephoneIsValid(String telephone){
-        if(telephone.length() != 10){
-            throw new IllegalArgumentException("telephone is invalid");
+        if(telephone.length() != 11){
+            throw new IllegalArgumentException("telephone invalid");
         }
     }
 
     private void ageIsValid(int age){
         if(age > 120){
-            throw new IllegalArgumentException("telephone is invalid");
+            throw new IllegalArgumentException("age invalid");
         }
     }
 }
