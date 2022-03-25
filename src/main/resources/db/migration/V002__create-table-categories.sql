6e14099499af2618a967389d1c24ef5a4b595b48
unknown <jacksonanderson1515@hotmail.com>
2022-03-25 14:09:10 -0300

    correction in migration V002

--- src/main/resources/db/migration/V002__create-table-categories.sql
+++ src/main/resources/db/migration/V002__create-table-categories.sql
@@ -1,5 +1,5 @@
 CREATE SEQUENCE category_id_sequence INCREMENT 1;
-CREATE TABLE users(
+CREATE TABLE categories(
     category_id BIGINT DEFAULT nextval('category_id_sequence') PRIMARY KEY,
     name VARCHAR(60) NOT NULL
 );
