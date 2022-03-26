7aac21d912243eac7b276141610a136076b2591c
unknown <jacksonanderson1515@hotmail.com>
2022-03-26 08:48:53 -0300

    added @Data in Category

--- src/main/java/com/bookstoreapi/bookstoreapi/categories/Category.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/categories/Category.java
@@ -1,8 +1,11 @@
 package com.bookstoreapi.bookstoreapi.categories;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Table(name = "categories")
+@Data
 @Entity
 public class Category {
 
--- src/main/java/com/bookstoreapi/bookstoreapi/categories/CategoryController.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/categories/CategoryController.java
@@ -1,11 +1,11 @@
 package com.bookstoreapi.bookstoreapi.categories;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 
@@ -18,12 +18,19 @@ public class CategoryController {
 
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Category> list(){
         return categoryService.findAll();
     }
 
     @GetMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.OK)
     public Category find(@PathVariable Long categoryId){
         return categoryService.findById(categoryId);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> entityNotFound(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
