26fbeca506fdbd504b1710a70f691d8c8f47c66c
unknown <jacksonanderson1515@hotmail.com>
2022-03-26 08:36:20 -0300

    inserted categories

---
+++ src/main/resources/db/migration/V005__insert-categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (name) VALUES('Romance');
+INSERT INTO categories (name) VALUES('Action');
+INSERT INTO categories (name) VALUES('Fantasy');
+INSERT INTO categories (name) VALUES('Fiction');
+INSERT INTO categories (name) VALUES('Horror');
+INSERT INTO categories (name) VALUES('Comedy');
+INSERT INTO categories (name) VALUES('Romantic Comedy');
+INSERT INTO categories (name) VALUES('Science Fiction');
+INSERT INTO categories (name) VALUES('Adventure');
+INSERT INTO categories (name) VALUES('Drama');
+
