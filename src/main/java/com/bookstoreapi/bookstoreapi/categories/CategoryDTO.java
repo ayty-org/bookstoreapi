61356e419388f633395a6fabea5b01a95e7fcb3e
unknown <jacksonanderson1515@hotmail.com>
2022-03-30 15:52:02 -0300

    added fields validation in CategoryDTO

--- src/main/java/com/bookstoreapi/bookstoreapi/categories/CategoryDTO.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/categories/CategoryDTO.java
@@ -2,12 +2,15 @@ package com.bookstoreapi.bookstoreapi.categories;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class CategoryDTO {
 
+    @NotBlank
     private String name;
 
     public CategoryDTO(Category category){
--- src/main/java/com/bookstoreapi/bookstoreapi/exception/ApiExceptionHandler.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/exception/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.bookstoreapi.bookstoreapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +20,11 @@ public class ApiExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> illegalArgumentHandler(IllegalArgumentException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getBindingResult().getAllErrors()
+                .get(0).getDefaultMessage());
     }
 }
