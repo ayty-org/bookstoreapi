b8d3ce9e593bed256af3aab5adc7cef145792b66
unknown <jacksonanderson1515@hotmail.com>
2022-03-31 09:39:22 -0300

    remove unused imports

--- src/main/java/com/bookstoreapi/bookstoreapi/client/service/GetClientService.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/client/service/GetClientService.java
@@ -2,8 +2,6 @@ package com.bookstoreapi.bookstoreapi.client.service;
 
 import com.bookstoreapi.bookstoreapi.client.ClientDTO;
 
-import java.util.List;
-
 @FunctionalInterface
 public interface GetClientService {
 
