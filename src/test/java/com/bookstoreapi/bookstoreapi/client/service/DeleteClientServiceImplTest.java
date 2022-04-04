d994b79464bfe2768df1313050fda4421e552c8e
unknown <jacksonanderson1515@hotmail.com>
2022-04-04 14:23:43 -0300

    DeleteClientServiceImpl test implemented

---
+++ src/test/java/com/bookstoreapi/bookstoreapi/client/service/DeleteClientServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.bookstoreapi.bookstoreapi.client.service;
+
+import com.bookstoreapi.bookstoreapi.client.Client;
+import com.bookstoreapi.bookstoreapi.client.ClientRepository;
+import com.bookstoreapi.bookstoreapi.purchase.service.PurchaseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@MockitoSettings
+public class DeleteClientServiceImplTest {
+
+    @InjectMocks
+    private DeleteClientServiceImpl deleteClientService;
+    @Mock
+    private ClientRepository repository;
+    @Mock
+    private PurchaseService purchaseService;
+    @Mock
+    private ClientService service;
+    private final Map<Long, Client> clients = new HashMap<>();
+
+
+    @BeforeEach
+    void setUp() {
+        Client client1 = new Client();
+        client1.setId(1L);
+        client1.setName("Jenipapo da Silva");
+
+        Client client2 = new Client();
+        client2.setId(2L);
+        client2.setName("Fernandin dos Santos");
+
+        clients.put(1L, client1);
+        clients.put(2L, client2);
+    }
+
+    @Test
+    void deleteWhenIdExistTest(){
+        when(purchaseService.existsByClientId(anyLong())).thenReturn(false);
+        when(service.findById(1L)).thenReturn(clients.get(1L));
+        deleteClientService.delete(1L);
+    }
+
+    @Test
+    void deleteWhenExistPurchaseWithClient(){
+        when(purchaseService.existsByClientId(anyLong())).thenReturn(true);
+        assertThrows(DataIntegrityViolationException.class, ()-> deleteClientService.delete(1L));
+    }
+}
