764ce0b2b93874c0f3beb0eb81d6832eb7dd9e61
unknown <jacksonanderson1515@hotmail.com>
2022-04-05 14:54:20 -0300

    PostPurchaseServiceImpl test implemented

---
+++ src/test/java/com/bookstoreapi/bookstoreapi/purchase/service/PostPurchaseServiceImplTest.java
@@ -0,0 +1,74 @@
+package com.bookstoreapi.bookstoreapi.purchase.service;
+
+import com.bookstoreapi.bookstoreapi.book.Book;
+import com.bookstoreapi.bookstoreapi.client.Client;
+import com.bookstoreapi.bookstoreapi.purchase.Purchase;
+import com.bookstoreapi.bookstoreapi.purchase.PurchaseDTO;
+import com.bookstoreapi.bookstoreapi.purchase.PurchaseRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class PostPurchaseServiceImplTest {
+
+    @InjectMocks
+    private PostPurchaseServiceImpl postPurchaseService;
+    @Mock
+    private PurchaseRepository repository;
+    @Mock
+    private PurchaseService purchaseService;
+
+
+
+    @Test
+    void saveTest(){
+        Purchase purchase = new Purchase();
+        purchase.setId(1L);
+
+        Book book = new Book();
+        book.setTitle("a");
+        book.setPrice(50);
+        book.setQuantityInStock(1);
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        purchase.setPurchasedBooks(books);
+
+        Client client = new Client();
+        client.setId(1L);
+        purchase.setClient(client);
+
+
+        PurchaseDTO purchaseDTO = new PurchaseDTO(purchase);
+        Client client2 = new Client();
+        client2.setId(2L);
+        client2.setName("Ana Júlia");
+
+        purchaseDTO.setClient(client2);
+
+        when(repository.save(purchase)).thenReturn(purchase);
+        when(purchaseService.getClient(client2)).thenReturn(client2);
+        when(purchaseService.getBooks(any())).thenReturn(books);
+
+        assertInstanceOf(PurchaseDTO.class, postPurchaseService.save(purchaseDTO));
+        assertEquals("Ana Júlia", postPurchaseService.save(purchaseDTO).getClient().getName());
+    }
+
+    @Test
+    void saveWhenFieldsDontExist(){
+        when(purchaseService.getClient(any())).thenThrow(EntityNotFoundException.class);
+        when(purchaseService.getBooks(any())).thenThrow(EntityNotFoundException.class);
+
+        assertThrows(EntityNotFoundException.class, ()->postPurchaseService.save(new PurchaseDTO()));
+    }
+}
