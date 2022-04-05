808d3395157bf42508b2100ecf281cbfaecab944
unknown <jacksonanderson1515@hotmail.com>
2022-04-05 13:05:35 -0300

    GetAllByCategoryNameBookServiceImpl test implemented

---
+++ src/test/java/com/bookstoreapi/bookstoreapi/book/service/GetAllByCategoryNameBookServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.bookstoreapi.bookstoreapi.book.service;
+
+import com.bookstoreapi.bookstoreapi.book.Book;
+import com.bookstoreapi.bookstoreapi.book.BookDTO;
+import com.bookstoreapi.bookstoreapi.book.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class GetAllByCategoryNameBookServiceImplTest {
+
+    @InjectMocks
+    private GetAllByCategoryNameBookServiceImpl getAllByCategoryNameBookService;
+    @Mock
+    private BookRepository bookRepository;
+    private final List<Book> allBooks = new LinkedList<>();
+
+
+    @BeforeEach
+    void setUp(){
+        Book book1 = new Book();
+        book1.setId(1L);
+        book1.setTitle("Book 1");
+
+        Book book2 = new Book();
+        book2.setId(2L);
+        book2.setTitle("Book 2");
+
+        Book book3 = new Book();
+        book3.setId(3L);
+        book3.setTitle("Book 3");
+
+        allBooks.add(book1);
+        allBooks.add(book2);
+        allBooks.add(book3);
+    }
+
+    @Test
+    void findAllTest(){
+        when(bookRepository.findAllByCategoriesNameIgnoreCase(anyString())).thenReturn(allBooks);
+        List<BookDTO> listReturned = getAllByCategoryNameBookService.findAllByCategoriesName("test");
+        assertThat(3, is(equalTo(listReturned.size())));
+        for(int k = 0; k<3; k++){
+            assertThat(allBooks.get(k).getTitle(), is(equalTo(listReturned.get(k).getTitle())));
+        }
+    }
+}
