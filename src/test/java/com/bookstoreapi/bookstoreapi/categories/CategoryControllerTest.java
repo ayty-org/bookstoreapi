fda27bc6325b68eff48811c29d45a7cd3609961e
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-04-18 11:37:20 -0300

    category integration tests implemented

--- src/test/java/com/bookstoreapi/bookstoreapi/categories/CategoryControllerTest.java
+++ src/test/java/com/bookstoreapi/bookstoreapi/categories/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package com.bookstoreapi.bookstoreapi.categories;
 
 import com.bookstoreapi.bookstoreapi.BookstoreApiJacksonApplicationTests;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +10,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static java.util.Collections.get;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 class CategoryControllerTest extends BookstoreApiJacksonApplicationTests {
@@ -19,7 +23,7 @@ class CategoryControllerTest extends BookstoreApiJacksonApplicationTests {
     private MockMvc mockMvc;
     @Autowired
     private CategoryController categoryController;
-    private String url = "/categories"
+    private String url = "/categories";
 
 
     @BeforeEach
@@ -28,10 +32,28 @@ class CategoryControllerTest extends BookstoreApiJacksonApplicationTests {
     }
 
     @Test
-    void getAll(){
+    void getAll() throws Exception{
         mockMvc.perform(get(url))
-                .andExpect("")
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$[0].name", is("Romance")))
+                .andExpect(jsonPath("$[1].name", is("Action")))
+                .andExpect(jsonPath("$[2].name", is("Fantasy")));
+    }
+
+    @Test
+    void getWhenIdExist() throws Exception{
+        mockMvc.perform(get(url+"/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Romance")));
 
     }
 
+    @Test
+    void getWhenIdDontExist() throws Exception{
+        Assertions.assertThatThrownBy(() ->
+                        mockMvc.perform(get(url + "/10"))
+                                .andExpect(status().isNotFound()))
+                .hasMessageContaining("Category with id 10 not found");
+    }
 }
