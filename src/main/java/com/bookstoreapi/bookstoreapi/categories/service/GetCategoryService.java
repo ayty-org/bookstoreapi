9dea79340818c1cbb7f5184cb3ffecd133881465
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-04-20 16:37:08 -0300

    refactoring in category services

--- src/main/java/com/bookstoreapi/bookstoreapi/categories/service/GetCategoryService.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/categories/service/GetCategoryService.java
@@ -1,9 +1,10 @@
 package com.bookstoreapi.bookstoreapi.categories.service;
 
 import com.bookstoreapi.bookstoreapi.categories.Category;
+import com.bookstoreapi.bookstoreapi.exception.CategoryNotFoundException;
 
 @FunctionalInterface
 public interface GetCategoryService {
 
-    Category findById(Long id);
+    Category findById(Long id) throws CategoryNotFoundException;
 }
--- src/main/java/com/bookstoreapi/bookstoreapi/categories/service/GetCategoryServiceImpl.java
+++ src/main/java/com/bookstoreapi/bookstoreapi/categories/service/GetCategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.bookstoreapi.bookstoreapi.categories.service;
 import com.bookstoreapi.bookstoreapi.categories.Category;
 import com.bookstoreapi.bookstoreapi.categories.CategoryDTO;
 import com.bookstoreapi.bookstoreapi.categories.CategoryRepository;
+import com.bookstoreapi.bookstoreapi.exception.CategoryNotFoundException;
 import com.bookstoreapi.bookstoreapi.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,7 @@ public class GetCategoryServiceImpl implements GetCategoryService{
     private final CategoryRepository categoryRepository;
 
     @Override
-    public Category findById(Long id) throws EntityNotFoundException {
-        return categoryRepository.findById(id).orElseThrow( () ->{
-            throw new EntityNotFoundException(id, CategoryDTO.getClassName());
-        });
+    public Category findById(Long id) throws CategoryNotFoundException {
+        return categoryRepository.findById(id).orElseThrow(() -> new CategoryNotFoundException(id));
     }
 }
